c094fd63bf9742f215b1980fc986b57960d9cc27
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-08-08 19:53:09 +0200

    Up

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""A recursive function that queries the Reddit
-API and returns a list containing the titles of
-all hot articles for a given subreddit."""
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """returns a list containing the titles
-    of all hot articles for a given subreddit"""
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+       "User-Agent": "linux:0x16.api.advanced:v1.0.0\
+            (by /u/Large_Alternative_30)"
     }
     params = {
         "after": after,
